9cc4a5877ad9caff7c161bba317cb83eca7dae57
terria1020 <jaehan1346@gmail.com>
2023-03-09 20:37:42 +0900

    docs: 서버 실행/종료 스크립트 추가

---
+++ scripts/app/kill.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 프로세스 검사
+check=$(ps -ef |grep "java -jar /root/github/api-server/build/libs/demo-0.0.1-SNAPSHOT.jar" |grep -v "grep" |wc |awk '{print $1}')
+# ps -ef: 프로세스 검색
+# grep: 문자열 필터 - 문자열 포함하는 행
+# grep -v 문자열 필터 - 문자열 제외하는 행
+# wc: word count
+# awk: 특정 열 출력
+
+if [ $check -gt 0 ] #서버가 실행중이면
+then
+    echo "kill api server."
+    ps -ef |grep "java -jar /root/github/api-server/build/libs/demo-0.0.1-SNAPSHOT.jar" |grep -v "grep"|head -n 1 |awk '{print $2}' |xargs -t kill -9
+else # 서버가 실행 중이지 않으면
+    echo "running api server not found. exit."
+fi
+
+# head -n 1: 파일의 처음부터 n번째 줄까지 출력
+# xargs: stdout를 다음에 오는 명령어의 stdin으로 입력
