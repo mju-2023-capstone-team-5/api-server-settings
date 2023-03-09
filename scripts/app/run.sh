7ae5df29196fd6642ded4a773e9738618c93f021
terria1020 <jaehan1346@gmail.com>
2023-03-09 21:45:19 +0900

    fix: run.sh에서 jar파일의 이름이 잘못 명시된 문제 수정

--- scripts/app/run.sh
+++ scripts/app/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup java -jar /root/github/api-server/build/libs/api-server-0.0.1-SNAPSHOT.jar.jar 1> nohup.out 2>&1 &
+nohup java -jar /root/github/api-server/build/libs/api-server-0.0.1-SNAPSHOT.jar 1> nohup.out 2>&1 &
 # Github Actions에서 ssh를 통해 실행되는 nohup는
 # stdin, stdout, stderr을 actions output가 가져가서 actions가 끝나지 않기 때문에
 # 2>&1을 통해 모든 출력이 nohup로 넘어가게 설정
