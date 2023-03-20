2d0218bd3e753640df45a55dda847660c929e165
terria1020 <jaehan1346@gmail.com>
2023-03-20 20:32:35 +0900

    fix: basic-setting에 git setting이 들어가 있는 내용 삭제

--- scripts/server/basic-setting.sh
+++ scripts/server/basic-setting.sh
@@ -3,21 +3,3 @@
 apt-get update
 apt-get upgrade -y
 apt-get install openjdk-11-jdk -y
-
-git config --global user.name "terria1020"
-git config --global user.email "jaehan1346@gmail.com"
-
-cd ~/
-mkdir app
-mkdir github
-
-cd ~/github
-git clone https://$GIT_ID:$GIT_TOKEN@github.com/mju-2023-capstone-team-5/api-server-settings.git
-cd api-server-settings
-cp scripts/app/*.sh ~/app/
-chmod +x ~/app/*.sh
-
-cd ~/github
-git clone https://$GIT_ID:$GIT_TOKEN@github.com/mju-2023-capstone-team-5/api-server.git
-git checkout develop
-git pull
