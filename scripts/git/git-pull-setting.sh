5b21369937b8c72ed68548e0a9bfaf965d899ef8
terria1020 <jaehan1346@gmail.com>
2023-03-20 20:29:36 +0900

    feat: remote 수정 대신 처음부터 끝까지 git repo 환경 세팅을 하도록 변경

--- scripts/git/git-pull-setting.sh
+++ scripts/git/git-pull-setting.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-cd /root/github/api-server
 
-git remote remove origin
-git remote add origin https://$GIT_ID:$GIT_TOKEN@github.com/mju-2023-capstone-team-5/api-server.git
+git config --global user.name "terria1020"
+git config --global user.email "jaehan1346@gmail.com"
+
+cd ~/
+mkdir app
+mkdir github
+
+cd ~/github
+git clone https://$GIT_ID:$GIT_TOKEN@github.com/mju-2023-capstone-team-5/api-server-settings.git
+cd api-server-settings
+cp scripts/app/*.sh ~/app/
+chmod +x ~/app/*.sh
+
+cd ~/github
+git clone https://$GIT_ID:$GIT_TOKEN@github.com/mju-2023-capstone-team-5/api-server.git
+git checkout develop
+git pull
+
+#cd /root/github/api-server
+
+
+#git remote remove origin
+#git remote add origin https://$GIT_ID:$GIT_TOKEN@github.com/mju-2023-capstone-team-5/api-server.git
 # Github Actions에서 private repo의 pull을 아이디 비밀번호 입력 없이 사용하기 위해 remote에 명시
+## -> clone부터 id:token 형식으로 clone하면 remote에 알아서 붙음
