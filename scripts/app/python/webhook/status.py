09140c82b2dbcdb81c8500767535fc0787d99888
terria1020 <jaehan1346@gmail.com>
2023-05-03 13:22:59 +0900

    feat: status.py에서 사용하지 않는 ping 관련 코드 삭제

--- scripts/app/python/webhook/status.py
+++ scripts/app/python/webhook/status.py
@@ -10,18 +10,8 @@ data = '{ "content": "[Github Actions] 배포 완료, 서버 실행 중" }'
 data_err = '{ "content": "[Github Actions] 배포에 오류가 있거나 서버가 실행 중이 아님" }'
 
 try:
-    response = requests.get("http://localhost:8080/actuator/health")
+    response = requests.get("http://localhost:8080/")
 except Exception as e:
     requests.post(discord, headers=headers, data=data_err.encode('utf-8'))
     exit(1)
-
 requests.post(discord, headers=headers, data=data.encode('utf-8'))
-
-ping = response.json()["components"]["ping"]["status"]
-db = response.json()["components"]["db"]["status"]
-
-response_content = "ping: " + ping + ", database: " + db
-
-data_response = '{ "content": \"' + response_content + '\" }'
-# response = requests.post(discord, headers=headers, data=data_response)
-# ping에 대한 정보는 나중에 하는걸로
