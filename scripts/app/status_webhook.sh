9d3a503e1828c9adb178c7a2b0000d7c717ebc14
terria1020 <jaehan1346@gmail.com>
2023-05-03 13:30:12 +0900

    feat: 배포 대기시간을 환경변수로 관리하게 변경

--- scripts/app/status_webhook.sh
+++ scripts/app/status_webhook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "sleep 30 sec for init server..."
-sleep 30
+echo "sleep $DEPLOY_DELAY sec for init server..."
+sleep $DEPLOY_DELAY
 
 echo "test status and send it to discord webhook"
 python3 /root/app/python/webhook/status.py
